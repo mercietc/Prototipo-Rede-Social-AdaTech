75e8b58fa3e6ef37d3520080dce21232960903e1
Mercia <68623425+mercietc@users.noreply.github.com>
2022-12-10 11:52:07 -0300

    Atualização de classes Pessoa, Usuario e Post

--- Social_Media/src/main/java/com/br/ada/modelo/Usuario.java
+++ Social_Media/src/main/java/com/br/ada/modelo/Usuario.java
@@ -3,30 +3,57 @@ package com.br.ada.modelo;
 import java.time.LocalDate;
 
 public class Usuario extends Pessoa{
-    private String login;
+    private int id;
+    private String nomeUsuario;
+    private String email;
     private String senha;
+    private LocalDate dataCriacao;
 
     public Usuario(){
 
     }
 
-    public Usuario(String login, String senha) {
-        this.login = login;
+    public Usuario(int id, String nomeUsuario, String email, String senha, LocalDate dataCriacao) {
+        this.id = id;
+        this.nomeUsuario = nomeUsuario;
+        this.email = email;
         this.senha = senha;
+        this.dataCriacao = dataCriacao;
     }
 
-    public Usuario(int id, String nome, LocalDate dataNasc, String cpf, String regiaoOrigem, String regiaoMorada, String login, String senha) {
-        super(id, nome, dataNasc, cpf, regiaoOrigem, regiaoMorada);
-        this.login = login;
+    public Usuario(int id, String nome, LocalDate dataNascimento, String profissao, LocalDate dataCriacao, int id1, String nomeUsuario, String email, String senha, LocalDate dataCriacao1) {
+        super(id, nome, dataNascimento, profissao, dataCriacao);
+        this.id = id1;
+        this.nomeUsuario = nomeUsuario;
+        this.email = email;
         this.senha = senha;
+        this.dataCriacao = dataCriacao1;
     }
 
-    public String getLogin() {
-        return login;
+    @Override
+    public int getId() {
+        return id;
     }
 
-    public void setLogin(String login) {
-        this.login = login;
+    @Override
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getSenha() {
@@ -38,10 +65,23 @@ public class Usuario extends Pessoa{
     }
 
     @Override
+    public LocalDate getDataCriacao() {
+        return dataCriacao;
+    }
+
+    @Override
+    public void setDataCriacao(LocalDate dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    @Override
     public String toString() {
         return "Usuario{" +
-                "Login: '" + login + '\'' +
-                ", Senha: '" + senha + '\'' + //dado sensível que deve ser escondido
+                "Id: " + id +
+                ", Nome de usuário: '" + nomeUsuario + '\'' +
+                ", E-mail: " + email + '\'' +
+                ", Senha: '" + senha + '\'' +
+                ", Criado em: " + dataCriacao +
                 '}';
     }
 }
